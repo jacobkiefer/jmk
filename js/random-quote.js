a3a89ac1c4f772dbbea7a10c382c67240fcfdc64
jacobkiefer <jacob.kiefer@gmail.com>
2016-08-08 08:56:12 -0500

    Add files via upload

---
+++ js/random-quote.js
@@ -0,0 +1,129 @@
+quotes = [];
+authors = [];
+
+var i = 0;
+
+quotes[i] = "Getting kicked in the teeth is the reward for doing good work.";
+authors[i++] = "John Boyd";
+quotes[i] = "You gotta challenge all assumptions. If you don’t, what is doctrine on day one becomes dogma forever after.";
+authors[i++] = "John Boyd";
+quotes[i] = "To be somebody or to do something. In life there is often a roll call. That’s when you will have to make a decision. To be or to do?";
+authors[i++] = "John Boyd";
+
+//quotes[i] = "Reality is the leading cause of stress for those in touch with it.";
+//authors[i++] = "Jack Wagner";
+
+//quotes[i] = "Few things are harder to put up with than the annoyance of a good example.";
+//authors[i++] = "Mark Twain";
+
+quotes[i] = "It's a strange world. Let's keep it that way";
+authors[i++] = "Warren Ellis";
+
+//quotes[i] = "We are not children of celestial fuckin' light, walkin' arm-in-arm into the Age of Aquarius. We are wankers who wreck the planet an' piss on each other, 'til half the world's starvin' an' the other half's busy findin' new ways to keep from noticin' it. That's the fuckin' limit've our potential, believe me.";
+//authors[i++] = "John Constantine";
+quotes[i] = " I've already blown my cover, so I may as well drop my pants and bugger it properly.";
+authors[i++] = "John Constantine";
+
+//quotes[i] = "Eating a donut is the easiest way to tell the world you don't give a fuck";
+//authors[i++] = "Bill Burr";
+//quotes[i] = "Real racism is quiet. It’s subtle. People look around first. They make sure the coast is clear. There’s disclaimers, like, “Dude, you know I’m not racist, but, uh, these,’insert group name,’ ‘followed by fucked-up conversation,’ right?";
+//authors[i++] = "Bill Burr";
+//quotes[i] = "You wanna know how you know you’re informed as a protestor? They don’t show your interview on TV.";
+//authors[i++] = "Bill Burr";
+//quotes[i] = "What’s that, son? Nah, we’re not going to church today, fuck that. It’s all a bunch of bullshit. God’s everywhere, but I gotta go down there to see him? Really? And he’s mad at me down there, and I owe you money? Go fuck yourself.";
+//authors[i++] = "Bill Burr";
+quotes[i] = "There's no risk when you go after a dream. There's a tremendous amount of risk to playing it safe";
+authors[i++] = "Bill Burr";
+
+quotes[i] = "\"I'm bored\" is a useless thing to say. You live in a great, big, vast world that you've seen none percent of. And even the inside of  your own mind is endless. It goes on forever inwardly. Do you understand? Being the fact that you're alive is amazing, so you don't get to be bored.";
+authors[i++] = "Louis C.K.";
+quotes[i] = "I think you have to try and fail, because failure gets you closer to what you're good at.";
+authors[i++] = "Louis C.K.";
+quotes[i] = "America's a family. We all yell at each other. It all works out.";
+authors[i++] = "Louis C.K.";
+
+//quotes[i] = "The death penalty is like the McRib. When you can't have it, it seems so tantalizing. But when they bring it back, you think, Wow, this is ethically wrong.";
+//authors[i++] = "John Oliver";
+//quotes[i] = "At this point, we aren't just flirting with disaster; we're rounding third base and asking if disaster has any condoms.";
+//authors[i++] = "John Oliver";
+//quotes[i] = "Economics is like the Dutch language - I'm told it makes sense, but I have my doubts.";
+//authors[i++] = "John Oliver";
+//quotes[i] = "America doesn't need libraries; you don’t need books here. There are plenty of books in the world, and plenty of people who've read them. It's not your area of expertise.... Stick to what you are truly great at -- TV.";
+//authors[i++] = "John Oliver";
+
+quotes[i] = "Somewhere, something incredible is waiting to be known.";
+authors[i++] = "Carl Sagan";
+quotes[i] = "Imagination will often carry us to worlds that never were. But without it we go nowhere.";
+authors[i++] = "Carl Sagan";
+quotes[i] = "Science is a way of thinking much more than it is a body of knowledge.";
+authors[i++] = "Carl Sagan";
+quotes[i] = "Education on the value of free speech and the other freedoms reserved by the Bill of Rights, about what happens when you don't have them, and about how to exercise and protect them, should be an essential prerequisite for being an American citizen... If we can't think for ourselves, if we're unwilling to question authority, then we're just putty in the hands of those in power. But if the citizens are educated and form their own opinions, then those in power work for us. In every country, we should be teaching our children the scientific method and the reasons for a Bill of Rights.";
+authors[i++] = "Carl Sagan";
+
+
+quotes[i] = "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.";
+authors[i++] = "Albert Einstein";
+//quotes[i] = "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.";
+//authors[i++] = "Albert Einstein";
+quotes[i] = "It's not that I'm so smart, it's just that I stay with problems longer.";
+authors[i++] = "Albert Einstein";
+
+quotes[i] = "I don't get high, but sometimes I wish I did. That way, when I messed up in life I would have an excuse. But right now there's no rehab for stupidity.";
+authors[i++] = "Chris Rock";
+//quotes[i] = "You know what they say, \"There's no reason to ever hit a woman.\" Shit! There's a reason to hit everybody. You just don't do it. Shit, there's a reason to kick an old man down a flight of stairs. You just don't do it. Ain't nobody above an ass-whooping.";
+//authors[i++] = "Chris Rock";
+quotes[i] = "Only dumb people try to impress smart people. Smart people just do what they do.";
+authors[i++] = "Chris Rock";
+
+//quotes[i] = "The Bible’s too wordy. The 10 Commandments are a load of shit. You don’t need all these things. The Bible should be one sheet of paper, and on that sheet of paper it should say: ‘Try not to be a cunt.’";
+//authors[i++] = "Jim Jefferies";
+//quotes[i] = "A wise man once said, \"Don't be a cunt.\"";
+//authors[i++] = "Jim Jefferies";
+quotes[i] = "If you want to get ahead in this world. Walk into that room and ask \"Why isn't that glass fucking full?\"";
+authors[i++] = "Jim Jefferies";
+
+quotes[i] = "If you can’t joke about the most horrendous things in the world, what’s the point of jokes? What’s the point in having humor? Humor is to get us over terrible things.";
+authors[i++] = "Ricky Gervais";
+quotes[i] = "People who criticize you have usually never achieved anywhere near what you have. Most of them would be too scared to even try. Keep going.";
+authors[i++] = "Ricky Gervais";
+//quotes[i] = "Mondays are fine. It’s your life that sucks.";
+//authors[i++] = "Ricky Gervais";
+quotes[i] = "If you spend your days doing what you love, it is impossible to fail.";
+authors[i++] = "Ricky Gervais";
+quotes[i] = "You should bring something into the world that wasn’t in the world before. It doesn’t matter what that is. It doesn’t matter if it’s a table or a film or gardening—everyone should create. You should do something and then sit back and say, ‘I did that.’";
+authors[i++] = "Ricky Gervais";
+//quotes[i] = "The best advice I’ve ever received is, ‘No one else knows what they’re doing either.'";
+//authors[i++] = "Ricky Gervais";
+//quotes[i] = "Life is just a series of peaks and troughs. And you don’t know whether you’re in a trough until you’re climbing out, or on a peak until you’re coming down. And that’s it you know, you never know what’s round the corner. But it’s all good. ‘If you want the rainbow, you’ve gotta put up with the rain.’ Do you know which ‘philosopher’ said that? Dolly Parton. And people say she’s just a big pair of tits.";
+//authors[i++] = "Ricky Gervais";
+quotes[i] = "Honor is a gift a man gives himself. You can be as good as anyone that ever lived. If you can read, you can learn everything that anyone ever learned. But you’ve got to want it.";
+authors[i++] = "Ricky Gervais";
+
+quotes[i] = "I swear to god I had something for this.";
+authors[i++] = "Sterling Archer";
+//quotes[i] = "I'm scared if I stop all at once, the cumulative hangover will literally kill me.";
+//authors[i++] = "Sterling Archer";
+
+//quotes[i] = "You know what cheers me up? Other people’s misfortune.";
+//authors[i++] = "Bender";
+//quotes[i] =  "Have you tried turning off the TV, sitting down with your children, and hitting them?";
+//authors[i++] = "Bender";
+//quotes[i] = "Afterlife? If I thought I had to live another life, I’d kill myself right now!";
+//authors[i++] = "Bender";
+
+quotes[i] = "I never had a policy; I have just tried to do my very best each and every day.";
+authors[i++] = "Abraham Lincoln";
+
+quotes[i] = "The best way to cheer yourself up is to try to cheer somebody else up.";
+authors[i++] = "Mark Twain";
+
+
+
+
+
+
+//calculate a random index number
+index = Math.floor(Math.random() * quotes.length);
+
+//display the quotation
+document.getElementById("quote").innerHTML = "<DT>" + "Random Quote:\n" + "</DT>"+"\"" + quotes[index] + "\"\n" + "<DD>" + "-- " + authors[index] + "</DT>\n";
